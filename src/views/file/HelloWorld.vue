c473bbc3cf15b94a3e02778bfb34da2a3e4c6c3b
刘群 <517067180@qq.com>
2023-03-13 09:01:05 +0800

    test

--- src/views/file/HelloWorld.vue
+++ src/views/file/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} Debug={{ debug }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +35,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    const Password = process.env.PASSWORD ?? 'abc'
+    return {
+      debug: Password
+    }
   }
 }
 </script>
